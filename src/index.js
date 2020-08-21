cfbf3b0e25a80779993f6641b212496fe02e4689
umermj2 <umermj2@gmail.com>
2020-08-21 08:27:19 +0500

    Hello Worl Stylings

--- src/index.js
+++ src/index.js
@@ -1,35 +1,66 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import "./index.css";
-function Room()
-{
-    const [isLit, setLit] = React.useState(true);
-    const [temp, setTemp] = React.useState(72);
-    const brightness = isLit ? "lit" : "dark";
-    return(
-        <div className={`room ${brightness}`}> The room is {isLit ? 'lit' : 'dark'}
-            <br />
-            <div>
+import './index.css';
+// import App from './App';
+// import * as serviceWorker from './serviceWorker';
 
-            <p>Temperatuer <sup>o</sup>F : <strong>{temp}</strong> </p>
-            <button onClick={() => setTemp(temp+1)}>
-                +
-            </button>
-            <button onClick={() => setTemp(temp-1)}>
-                -
-            </button>
-            </div>
-            <button onClick={() => setLit(!isLit)}>
-                flip
-            </button>
-            <button onClick={() => setLit(isLit?isLit:!isLit)}>
-                ON
-            </button>
-            <button onClick={() => setLit(isLit?!isLit:isLit)}>
-                OFF
-            </button>
-        </div>
-    );
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
+
+// function Hi(props) {
+// function Hi({ name }) {
+//   // return <div>Hello {props.name}!</div>;
+//   return <div>Hello {name}!</div>;
+// }
+// const Hi = ({ name }) => {
+//   return <div>Hello {name}!</div>;
+// }
+// Plain function:
+// function Hi({ name }) {
+//   return <div>Hello {name}!</div>;
+// }
+
+// A constant holding an anonymous function:
+// const Hi = function({ name }) {
+//   return <div>Hello {name}!</div>;
+// }
+
+// Turning the "function" into an arrow:
+// const Hi = ({ name }) => {
+//   return <div>Hello {name}!</div>;
+// }
+
+// Optional step 3: Removing the braces, which makes the
+// "return" implicit so we can remove that too. Leaving the parens
+// in for readability:
+const Hi = ({ name }) => (
+  <div className="custom-div">Hello <strong>{name}</strong>!
+    <ol>
+      <li>Umer</li>
+      <li>Zeeshan</li>
+      <li>Zia</li>
+      </ol>
+      {5 + 10}
+  </div>
+)
+
+// Optional step 4: If the component is really short, you can put
+// it all on one line, and skip the parentheses:
+// const Hi = ({ name }) => <div>Hello  <strong>{name}</strong>!</div>;
+
+function HiFullName(props) {
+  return (
+    <div>Hi {props.firstName} {props.lastName}!</div>
+  );
 }
 
-ReactDOM.render(<Room/>,document.querySelector('#root'));
+ReactDOM.render(<Hi name="Umer"/>, document.querySelector('#root'));
